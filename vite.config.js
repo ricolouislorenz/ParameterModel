de782152ede9429f27ad8db9f3ce2f28e0d8738f
Rico Lorenz <ricolouislorengmail.com>
2024-09-18 14:28:27 +0200

    fix vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/ParameterModel/', 
   plugins: [react()],
   server: {
-    open: true, 
-    cors: true 
-  }
-})
+    open: true,
+    cors: true,
+  },
+});
